f7f48685e3735f8c8a4a0d86f8d13181b5a22793
Tony Bowden <tony@tmtm.com>
2017-04-27 11:26:40 +0100

    Remove term_map code
    All the memberships at this URL are in term 6, so there's no need for
    all this.

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,7 @@
 # #!/bin/env ruby
 # encoding: utf-8
 
-require 'date'
+require 'pry'
 require 'scraped'
 require 'scraperwiki'
 
@@ -55,15 +55,15 @@ def fix_name(name)
   [name, honorific_prefix, honorific_suffix]
 end
 
-def scrape_list(url, term_map)
+def scrape_list(url)
   noko = noko_for(url)
   noko.css('div.delegate_list tr td a/@href').each do |link|
     bio = URI.join(url, link.to_s)
-    scrape_person(bio, term_map)
+    scrape_person(bio)
   end
 end
 
-def scrape_person(url, term_map)
+def scrape_person(url)
   noko = noko_for(url)
   details = noko.css('div.optimize')
 
@@ -102,67 +102,14 @@ def scrape_person(url, term_map)
     party:            party,
     start_date:       start_date,
     birth_date:       dob,
+    term:             6,
     source:           url.to_s,
   }
-
-  if !term_map[start_date].nil?
-    data[:term] = term_map[start_date]
-  else
-    term_start = ''
-    term_map.keys.sort.each do |start|
-      term_start = start if start < start_date
-    end
-    data[:term] = term_map[term_start]
-  end
 
   data = data.merge(social_details)
   # puts data.reject { |k,v| v.to_s.empty? }.sort_by { |k,v| k }.to_h
 
-  if data[:term].nil?
-    ScraperWiki.save_sqlite([:id], data)
-  else
-    ScraperWiki.save_sqlite(%i(id term), data)
-  end
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
-def create_terms(url)
-  noko = noko_for(url)
-  side_menu = noko.css('div.side_menu')
-  link = side_menu.xpath('.//li/a[contains(.,"Legislature Archive")]')
-
-  noko = noko_for(URI.join(url, link.first[:href]))
-  side_menu = noko.css('div.side_menu')
-  archives = side_menu.xpath('.//li/a[contains(.,"Legislature Archive")]/following-sibling::ul/li')
-
-  date_to_term_map = {}
-  count = 1
-  archives.reverse.each do |term|
-    name = term.text.tidy
-    start = name.gsub(' legislature', '')
-    start_date = Date.parse(start).to_s
-    term = {
-      name:       name,
-      start_date: start_date,
-      id:         count,
-      source:     URI.join(url, term.css('a/@href').to_s).to_s,
-    }
-    date_to_term_map[start_date] = count
-    ScraperWiki.save_sqlite([:id], term, 'terms')
-    count += 1
-  end
-  unless date_to_term_map['2016-06-03'].nil?
-    raise "the latest term has been archived, possibly there's been an election"
-  end
-  term = {
-    name:       '3 June 2016 legislature',
-    start_date: '2016-06-03',
-    id:         count,
-    source:     url,
-  }
-  date_to_term_map['2016-06-03'] = count
-  ScraperWiki.save_sqlite([:id], term, 'terms')
-  date_to_term_map
-end
-
-term_map = create_terms('http://www.parlament.gov.rs/national-assembly/composition/members-of-parliament/current-legislature.487.html')
-scrape_list('http://www.parlament.gov.rs/national-assembly/composition/members-of-parliament/current-legislature.487.html', term_map)
+scrape_list('http://www.parlament.gov.rs/national-assembly/composition/members-of-parliament/current-legislature.487.html')
